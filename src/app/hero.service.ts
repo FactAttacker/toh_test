f00cda6bd231104067c5ce609fc709394b36b8f6
박도현 <ehgusdl03@gmail.com>
2018-09-29 18:21:20 +0900

    test

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,8 @@ import {HEROES} from './mock-heroes';
 import {Hero} from './hero';
 import {Observable, of, Subject} from 'rxjs';
 import {delay} from 'rxjs/operators';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment.prod";
 
 // 1. (root 컴포넌트가 만들어질때) 해당 서비스를 컨테이너에 등록
 @Injectable({
@@ -15,19 +17,23 @@ export class HeroService {
   refresh$ = this.refresh.asObservable(); // subscriber: subscribe()로 데이터 수신
 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
     // 네트워크를 통해서 모델 정보 획득
     //return of(HEROES).pipe(delay(1000));
-    return of(HEROES);
+    return this.http.get<Hero[]>(environment.HOST+'/api/heroes');
+    //return of(HEROES);
   }
 
   getHero(hero_id: number): Observable<Hero>{
-    //네트워크를 통해서 모델 정보 획득
-    const h =  HEROES.find(hero =>  hero.id === hero_id ?  true :  false );
-    return of(h);
-    //return of(HEROES.find(hero => hero.id === hero_id));
+    // /api/hero/:hero_id 통해서 모델 정보 획득
+    const h =  HEROES.find(hero =>  hero.hero_id === hero_id ?  true :  false );
+    //return of(h);
+
+    //template string 문법 `${변수}`
+    return this.http.get<Hero>(`${environment.HOST}/api/heroes/${hero_id}`);
+    //return of(HEROES.find(hero => hero.hero_id === hero_id));
   }
 
 }
